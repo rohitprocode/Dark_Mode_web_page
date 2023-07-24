3ada18a2b7c49037b585e2110aac0a1a88d8fc8b
rohitprocode <rohit990797@gmail.com>
2023-07-25 00:06:48 +0530

    Completed

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let Body_color = document.getElementById('container');
+let isDark = true
 
-
-function dark(){
-    Body_color = document.getElementById('container').style.backgroundColor = "black";
-
-}
-
-function light(){
-    Body_color = document.getElementById('container').style.backgroundColor = "white";
+function toggleDarkAndWhite(){
+    if(isDark == true){
+        Body_color = document.getElementById('container').style.backgroundColor = "black";
+        isDark = false;
+    } else {
+        Body_color = document.getElementById('container').style.backgroundColor = "white";
+        isDark = true;
+    }
 }
