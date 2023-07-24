01b3210f30d8d5549bf3b69337fbeef7e8511189
rohitprocode <rohit990797@gmail.com>
2023-07-24 23:51:04 +0530

    Dark Mode

---
+++ script.js
@@ -0,0 +1,11 @@
+let Body_color = document.getElementById('container');
+
+
+function dark(){
+    Body_color = document.getElementById('container').style.backgroundColor = "black";
+
+}
+
+function light(){
+    Body_color = document.getElementById('container').style.backgroundColor = "white";
+}
